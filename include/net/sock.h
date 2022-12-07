bc1bb31e93b4b1db2fbfa4207411416eb778d14f
Menglong Dong <imagedong@tencent.com>
2022-12-07 10:29:15 +0800

    doc: tcp: add some document to tcp RTT

--- include/net/sock.h
+++ include/net/sock.h
@@ -516,7 +516,7 @@ struct sock {
 		struct timer_list	tcp_retransmit_timer;
 		struct timer_list	mptcp_retransmit_timer;
 	};
-	/* 每秒发送速率（bytes/s），由拥塞控制动态更新。 */
+	/* 计算出来的每秒发送速率（bytes/s），和MSS及RTT相关。 */
 	unsigned long		sk_pacing_rate; /* bytes per second */
 	atomic_t		sk_zckey;
 	atomic_t		sk_tskey;
